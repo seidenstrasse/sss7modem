20aff761e64ac07fbfb8a76f4bb4346b64294cb7
LongHairedHacker <sebastian@sebastians-site.de>
2016-11-30 22:47:26 +0100

    Added threads and mutexes for eventloop

--- software/linux/libsss7.c
+++ software/linux/libsss7.c
@@ -6,15 +6,20 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <termios.h>
+#include <pthread.h>
+
+#include "sss7.h"
+
 
 uint8_t uart_rx_byte;
-uint8_t uart_tx_byte, uart_has_tx_byte;
+_Atomic uint8_t uart_tx_byte, uart_has_tx_byte, uart_tx_done;
+
+pthread_t event_thread;
+pthread_mutex_t state_mutex, rx_buffer_mutex;
 
 int serial_fd;
 
-int libsss7_start(char *serialport) {
-	sss7_init();
-
+int uart_init(char *serialport) {
 	serial_fd = open(serialport, O_RDWR | O_NOCTTY | O_NDELAY);
 	if (serial_fd == -1) {
 	  perror("Error: Unable to open serialport");
@@ -45,18 +50,30 @@ int libsss7_start(char *serialport) {
 	options.c_cc[VMIN] = 0;
 	tcsetattr(serial_fd, TCSAFLUSH, &options);
 
-	int res = 0;
-	while(1) {
-		res = read(serial_fd, &uart_rx_byte, 1);
-		if(res == 1) {
-			sss7_process_rx();
-		}
+	return 0;
+}
 
-		if(uart_has_tx_byte) {
-			uart_has_tx_byte = 0;
-			write(serial_fd, &uart_has_tx_byte, 1);
-			sss7_process_tx();
-		}
+void *eventloop(void *arg);
+
+int libsss7_start(char *serialport) {
+	int res = 0;
+
+	sss7_init();
+	res = uart_init(serialport);
+	if(res) {
+		return -1;
+	}
+
+	uart_has_tx_byte = 0;
+	uart_tx_done = 0;
+
+	pthread_mutex_init(&state_mutex, NULL);
+	pthread_mutex_init(&rx_buffer_mutex, NULL);
+
+	res = pthread_create(&event_thread, NULL, eventloop, NULL);
+	if(res) {
+		printf("Could not create eventloop thread\n");
+		return -1;
 	}
 
 	return 0;
@@ -67,7 +84,9 @@ int libsss7_can_send(void) {
 }
 
 void libsss7_send(uint8_t msg[SSS7_PAYLOAD_SIZE]) {
-
+	pthread_mutex_lock(&state_mutex);
+	sss7_send(msg);
+	pthread_mutex_unlock(&state_mutex);
 }
 
 int libsss7_send_failed(void) {
@@ -79,10 +98,14 @@ int libsss7_has_received(void) {
 }
 
 void libsss7_get_received(uint8_t msg[SSS7_PAYLOAD_SIZE]) {
-	return sss7_get_received(msg);
+	pthread_mutex_lock(&rx_buffer_mutex);
+	sss7_get_received(msg);
+	pthread_mutex_unlock(&rx_buffer_mutex);
 }
 
 void libsss7_stop() {
+	pthread_join(event_thread, NULL);
+
 	close(serial_fd);
 }
 
@@ -93,4 +116,34 @@ uint8_t uart_get_byte(void) {
 void uart_put_byte(uint8_t byte) {
 	uart_has_tx_byte = 1;
 	uart_tx_byte = byte;
+}
+
+void *eventloop(void *arg) {
+	int res = 0;
+	while(1) {
+		if(uart_has_tx_byte) {
+			write(serial_fd, &uart_tx_byte, 1);
+			printf("Send %x\n", uart_tx_byte);
+			uart_has_tx_byte = 0;
+			uart_tx_done = 1;
+		}
+
+		res = read(serial_fd, &uart_rx_byte, 1);
+		if(res == 1) {
+			pthread_mutex_lock(&state_mutex);
+			pthread_mutex_lock(&rx_buffer_mutex);
+			sss7_process_rx();
+			pthread_mutex_unlock(&rx_buffer_mutex);
+			pthread_mutex_unlock(&state_mutex);
+		}
+
+		if(uart_tx_done) {
+			uart_tx_done = 0;
+			pthread_mutex_lock(&state_mutex);
+			sss7_process_tx();
+			pthread_mutex_unlock(&state_mutex);
+		}
+	}
+
+	return NULL;
 }
--- software/linux/libsss7.h
+++ software/linux/libsss7.h
@@ -3,18 +3,17 @@
 
 #include "sss7.h"
 
-
 int libsss7_start(char *serialport);
 
 int libsss7_can_send(void);
 
-void libsss7_send(uint8_t msg[SSS7_PAYLOAD_SIZE]);
+void libsss7_send(uint8_t *msg);
 
 int libsss7_send_failed(void);
 
 int libsss7_has_received(void);
 
-void libsss7_get_received(uint8_t msg[SSS7_PAYLOAD_SIZE]);
+void libsss7_get_received(uint8_t *msg);
 
 void libsss7_stop();
 
