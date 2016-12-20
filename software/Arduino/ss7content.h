ba4616e152fde6ce86b764eb18ae483fef601afb
Robert Nixdorf <mail@rnix.de>
2016-12-20 21:17:39 +0100

    added real stuff maybe wrong place to commit (sorry)

---
+++ software/Arduino/ss7content.h
@@ -0,0 +1,28 @@
+
+
+#ifndef ss7content_h
+#define ss7content_h
+
+//MSG ID
+
+#define MSG_DETECT          0x00
+#define MSG_CONNECT         0x01
+#define MSG_CONNECTED       0x02
+#define MSG_REQUEST_DIRECT  0x03
+#define MSG_REQUEST_BARCODE 0x04
+#define MSG_CONFIRM_REQUEST 0x05
+#define MSG_BEGIN_TRANSFER  0x06
+#define MSG_BARCODE_DETECT  0x07
+
+
+//STATION
+
+#define ID_READER   0x00
+#define ID_CENTRAL  0x01
+
+#define ID_ROUTER1  0x03
+
+#define ID_VAC      0xFF
+
+
+#endif /* ss7content_h */
