14fd4e5eb5c43a2d22131a7ba3b24ee37842534d
LongHairedHacker <sebastian@sebastians-site.de>
2016-12-22 17:55:00 +0100

    Totally the wrong place Revert "added real stuff maybe wrong place to commit (sorry)"
    This reverts commit ba4616e152fde6ce86b764eb18ae483fef601afb.

--- software/Arduino/main.cpp
+++ software/Arduino/main.cpp
@@ -1,57 +1,5 @@
 #include <Arduino.h>
 #include "ardusss7.h"
-#include "ss7content.h"
-#include <AccelStepper.h>
-
-#define ENDSTOP1 6
-#define S1_ENABLE 2
-#define MICROSTEP A5
-
-#define MAXPOSITIONS 6
-#define DISTANCE 1000
-uint16_t position[MAXPOSITIONS]{60,1060,2060,3085,4085,5085};
-
-#define ID 3
-
-void init_stepper(){
-    pinMode(ENDSTOP1, INPUT_PULLUP);
-    
-    pinMode(S1_ENABLE, OUTPUT); //ENABLE STEPPER
-    digitalWrite(S1_ENABLE, LOW);
-    
-    pinMode(MICROSTEP, OUTPUT);
-    digitalWrite(MICROSTEP, LOW);
-    
-    stepper.setAcceleration(1500);
-    stepper.setMaxSpeed(3000);
-    
-}
-
-void home(){
-    if(!digitalRead(ENDSTOP1)){
-        stepper.runToNewPosition(100);
-    }
-    uint16_t stepout = 0;
-    while(digitalRead(ENDSTOP1 && (stepout <= (DISTANCE*MAXPOSITIONS)))){
-        stepper.moveTo(-10);
-        stepout += 10;
-        stepper.run();
-        stepper.setCurrentPosition(0);
-        // stepper.runToNewPosition(0);
-        //stepper.run();
-        digitalWrite(11,LOW); //Kein plan
-    }
-}
-
-void select(uint8_t tube){
-    if(tube>0 && tube <= MAXPOSITIONS){
-        stepper.runToNewPosition(position(tube));
-    }
-    else{
-        home();
-    }
-}
-
 
 void setup() {
 	Serial.begin(9600);
@@ -80,25 +28,25 @@ void loop() {
 
 
     while(1) {
-		//Serial.println("Waiting to send");
-        //while(!SSS7.canSend());
-		//Serial.println("Sending ...");
-        //SSS7.send(msg);
-        //while(!SSS7.canSend());
+		Serial.println("Waiting to send");
+        while(!SSS7.canSend());
+		Serial.println("Sending ...");
+        SSS7.send(msg);
+        while(!SSS7.canSend());
 		//Serial.println("Finished Sending");
-        //if(SSS7.sendFailed()) {
-        //    Serial.println("Send failed");
-       // }
+        if(SSS7.sendFailed()) {
+            Serial.println("Send failed");
+        }
+
+
 		if(SSS7.hasReceived()) {
 			uint8_t msg1[SSS7_PAYLOAD_SIZE];
 			SSS7.getReceived(msg1);
 			Serial.print("Got data:");
 			Serial.println((char*)msg1);
-            if((msg1[0]==MSG_CONNECT)&&(msg[2]==ID)){
-                select(msg1[3]);
-            }
 		}
 
+        _delay_ms(1000);
     }
 
 };
